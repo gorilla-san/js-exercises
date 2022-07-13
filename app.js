d01d9d46c755341620c2fca5129e496b4706db2e
gorilla-san <108207000+gorilla-san@users.noreply.github.com>
2022-07-13 16:59:27 +0200

    asd

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 document.write("Hello World Bitch 2 Bitch 1 Bitch 0 ZUUUUZZZZZ ziz ziz ziz")
+var dugme = getElementsByClassName("peric")
+dugme.addEventListener("click", document.Write("YEEEESTE"))
